252f9a2e78f9b4162855b7b4469faa3f4fa92cc8
Danny Franken <Danny@192.168.2.2>
2013-03-21 08:21:35 +0100

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  test
+//
+//  Created by Danny Franken on 21-03-13.
+//  Copyright (c) 2013 Danny Franken. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
