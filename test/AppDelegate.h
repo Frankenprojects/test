0b37e99f534d3c6f00d7bbbfcc6af155a474aaa7
Danny Franken <danny@frankenprojects.com>
2013-03-23 20:08:38 +0100

    Revert "Clean it all!"
    This reverts commit a04ad802bfe3ce6a3e369a603374383c57ba2705.

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Danny Franken on 21-03-13.
+//  Copyright (c) 2013 Danny Franken. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
